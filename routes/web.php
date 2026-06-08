060c340f50b1cde0d9c241cffb3ed97159c9d6da
riffbyfadhil <fadhildangkua@gmail.com>
2026-06-08 14:50:36 +0700

    feat: login page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Enums\UserRole;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Web\AssetController;
 use App\Http\Controllers\Web\AssetGeofenceController;
 use App\Http\Controllers\Web\AssetMovementController;
@@ -20,6 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function (): void {
     Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.store');
 });
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
